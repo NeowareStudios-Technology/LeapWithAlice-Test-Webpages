37a4e882dfc1648c85025633ea2c37b1f8d46968
Leap With Alice™ <info@leapwithalice.io>
2018-06-21 13:58:47 -0400

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -402,6 +402,13 @@
 /******************************************************************************
 *                   NEW HEADER IMAGE ANIMATION                                *
 *******************************************************************************/
+var animation = bodymovin.loadAnimation({
+	container: document.getElementById('animatedHeader'),
+	renderer: 'svg',
+
+	autoplay: true,
+	path: 'assets/js/data.json'
+})
 
 
 
